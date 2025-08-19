2019039317091475bbad181295e18e28a1fca96c
Nicolas <nicolaslinaresoli@gmail.com>
2025-08-19 19:39:35 -0300

    Iniciando usuários

--- app/src/user/app.service.ts
+++ app/src/user/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(idade: number): string {
+    if (idade > 18){
+      return 'Maior de idade';
+    } else {
+      return 'Menor de idade';
+    }
   }
 }
