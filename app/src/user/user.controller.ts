073daa804c49e4237d2bf3fbe6c854b1b1734651
Nicolas <nicolaslinaresoli@gmail.com>
2025-08-20 00:25:15 -0300

    Crud iniciado

--- app/src/user/user.controller.ts
+++ app/src/user/user.controller.ts
@@ -1,11 +1,34 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { UserService } from './user.service';
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
-    @Controller('user')
-    export class UserController {
-        constructor(private readonly userService: UserService) {}
-        @Get(':id')
-        findOne(@Param('id') id: number){
+@Controller('user')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Get()
+    findAll() {
+        return this.userService.findAll();
+    }
+    
+    @Get(':id')
+    findOne(@Param('id') id: number){
         return this.userService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto) {
+        return this.userService.create(createUserDto);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto){
+        return this.userService.update(id, updateUserDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+        return this.userService.remove(id);
     }
 }
--- app/src/user/user.service.ts
+++ app/src/user/user.service.ts
@@ -1,8 +1,26 @@
 import { Injectable} from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Injectable()
 export class UserService {
+    findAll() {
+        return "Você está listando todos os usuários";
+    }
+    
     findOne(id: number) {
         return `Você está procurando o id: ${id}`;
+    }
+
+    create(createUserDto: CreateUserDto) {
+        return createUserDto;
+    }
+
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return {id, updateUserDto};
+    }
+
+    remove(id: number){
+        return `Você está removendo o usuário de id: ${id}`;
     }
 }
